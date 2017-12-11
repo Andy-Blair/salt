edfc3b9d887e50343ac4caca5d55c56231f7928e
andy <andy@andy.com>
2017-12-11 17:28:22 +0800

    1.发布更新方式由服务端先检出到本地，再分发至客户端，改为在添加项目时，生成更新脚本并推送到客户端，服务端采用远程执行更新脚本 2.更新页面实时显示的信息，由显示分发的文件列表，改为Git更新结果，以及所更新的版本的提交信息 3.注释掉了为兼容Python3编码的import 【from __future__ import unicode_literals】，此处导入会引起Python2的一些编码错误 4.自动发布服务端需要具有的文件和目录：/srv/salt/pkg/scripts/git_update.py 、/srv/salt/pkg/scripts/git_update_run.sls 、/srv/salt/pkg/scripts/git_update.sls 、/srv/salt/pkg/scripts/web_git/

--- salt_front/views.py
+++ salt_front/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.shortcuts import render_to_response, HttpResponse, HttpResponseRedirect, redirect
 from django.contrib.auth.decorators import login_required
@@ -13,7 +13,6 @@ from dwebsocket import accept_websocket
 import os
 import subprocess
 import sys
-
 
 # Create your views here.
 login_url = "/salt/login/"
@@ -59,111 +58,66 @@ def detail_socket(request,operate,web_id):
         for m in request.websocket:
             operate = operate
             web_info = Website.objects.get(website_id=web_id)
-            web_git_url = web_info.git_url
-            sync = False
+            web_url = web_info.url
+            sls_name = web_url.replace(".","_")
             re_tomcat = False
+            web_servers_info = web_info.server_id.values()
+            web_server_ip = []
+            for i in range(len(web_servers_info)):
+                web_server_ip.append(web_servers_info[i]["ipaddress"])
+            cli = client.LocalClient()
+            request.websocket.send("正在更新......\n\n")
+            for i in web_server_ip:
+                request.websocket.send(i.strip().encode('utf8') +":\n")
+                sync_re = cli.cmd(tgt=i.strip(), fun='state.sls', arg=['pkg.scripts.web_git.%s.%s_git' % (sls_name, sls_name)] )
+                print "update:",sync_re
+                def get_dval(dic,key):
+                    # get change files
+                    for k, v in dic.items():
+                        if k == key:
+                            return v
+                        else:
+                            if isinstance(v, dict):
+                                return get_dval(v,key)
 
-            # --- checkout code to local(/srv/salt/project) from remote. start ---
-            # pro = web_git_url.split("/")[1:]
-            # rel_git_url = "git@git.jingzhengu.com:jenkins/" + "/".join(pro)
-            request.websocket.send("正在获取更新文件......\n\n".encode('utf8'))
-            result = git_checkout.git_checkout(git_url=web_git_url,op=operate)
-            if result == 0:
-                sync = True
-                request.websocket.send("获取更新文件成功！\n\n".encode('utf8'))
-            else:
-                request.websocket.send("获取更新文件失败，请联系运维部！\n\n".encode('utf8'))
-            # --- checkout code to local(/srv/salt/project) from remote. end ---
+                result = get_dval(sync_re,"result")
+                if result:
+                    stdout = get_dval(sync_re,"stdout")
+                    request.websocket.send("  " + stdout + "\n")
+                    request.websocket.send("------更新完成！------\n\n")
+                    re_tomcat = True
+                else:
+                    stderr = get_dval(sync_re,"stderr")
+                    comment = get_dval(sync_re,"comment")
+                    request.websocket.send("错误信息：\n")
+                    request.websocket.send("  " + comment + "\n")
+                    request.websocket.send("  " + stderr + "\n")
 
-            if sync:
-                web_servers_info = web_info.server_id.values()
-                web_server_ip = []
-                web_server_ostype = None
-                for i in range(len(web_servers_info)):
-                    web_server_ip.append(web_servers_info[i]["ipaddress"])
-                    web_server_ostype = web_servers_info[i]["ostype"]
-                pro_name = web_git_url.split("/")[-1].split(".")[0]
-                web_path = web_info.path
+                # --- Read Tomcat Log. start ---
+                if web_info.type.lower() == "tomcat" and re_tomcat:
+                    # 配置远程服务器的IP，帐号，密码，端口等，因做了双机密钥信任，所以不需要密码
+                    r_user = "app"
+                    r_ip = i.strip()
+                    r_port = 22
+                    r_log = "/apps/product/tomcat/logs/catalina.out"  # tomcat的启动日志路径
+                    cmd_rlog = "/usr/bin/ssh -p {port} {user}@{ip} /usr/bin/tail -f {log_path}".format(user=r_user, ip=r_ip, port=r_port, log_path=r_log)
+                    cmd_tstart = "/usr/bin/ssh -p {port} {user}@{ip} /apps/product/tomcat/bin/startup.sh".format(user=r_user, ip=r_ip, port=r_port)
+                    p_rlog = subprocess.Popen(cmd_rlog, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                    tom_stop_re = cli.cmd(tgt=r_ip, fun='state.sls', arg=['pkg.scripts.tomcat_shutdown'])
+                    request.websocket.send("Tomcat has stopped\n\n")
+                    p_start = subprocess.Popen(cmd_tstart, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                    while p_start.poll() == None:
+                        start_l = p_start.stdout.readline()
+                        request.websocket.send(start_l)
 
-                # --- create pro_name.sls file and add call-command to top.sls. start ---
-                sls_files = os.listdir("/srv/salt/project")
-                if "%s.sls" % pro_name not in sls_files:
-                    if web_server_ostype.lower() == "windows":
-                        data = [pro_name + ":\n", "  file.recurse:\n", "    - name: %s\n" % web_path,
-                                "    - source: salt://project/%s\n" % pro_name, "    - mkdir: True\n"]
-                    else:
-                        data = [pro_name + ":\n", "  file.recurse:\n", "    - name: %s\n" % web_path,
-                                "    - source: salt://project/%s\n" % pro_name, "    - user: app\n","    - group: app\n",
-                                "    - file_mode: 644\n",
-                                "    - dir_mode: 755\n", "    - mkdir: True\n"]
-                    pro_sls = open('/srv/salt/project/%s.sls' % pro_name, str('w+'))
-                    pro_sls.writelines(data)
-                    pro_sls.close()
-                    top_sls = open('/srv/salt/top.sls', str('a+'))
-                    top_sls_data = str("    - project.%s" % pro_name)
-                    top_sls_content = top_sls.readlines()
-                    if top_sls_data not in top_sls_content:
-                        top_sls.write(top_sls_data)
-                    top_sls.close()
-                # --- create pro_name.sls file and add call-command to top.sls. end ---
+                    while p_rlog.poll() == None:
+                        re_log = p_rlog.stdout.readline()
+                        request.websocket.send(re_log)
+                        if "Server startup in" in re_log:
+                            break
+                    kill_tail_re = cli.cmd(tgt=r_ip, fun='state.sls', arg=['pkg.scripts.kill_tail'])
 
-                # --- sync files from server. start ---
-                cli = client.LocalClient()
-                request.websocket.send("正在更新......\n\n".encode('utf8'))
-                for i in web_server_ip:
-                    request.websocket.send(i.strip().encode('utf8')+":\n".encode('utf8'))
-                    sync_re = cli.cmd(tgt=i.strip(), fun='state.sls', arg=['project.%s' % pro_name])
-                    def get_change(dic):
-                        # get change files
-                        for k, v in dic.items():
-                            if k == "changes":
-                                return v
-                            else:
-                                if isinstance(v, dict):
-                                    return get_change(v)
-
-                    update_file = get_change(sync_re)
-                    if len(update_file) != 0:
-                        re_tomcat = True
-                        for k,v in update_file.items():
-                            # ch = v.items()[0][1]
-                            # v_ch = str("  ") + k + str(' ...... ') + ch + "\n"
-                            v_ch = str("  ") + k + "\n"
-                            request.websocket.send(v_ch.encode('utf8'))
-                    else:
-                        request.websocket.send("文件都是最新的，不需要更新！\n".encode('utf8'))
-                    request.websocket.send("\n".encode('utf8'))
-                # --- sync files from server. end ---
-
-                    # --- Read Tomcat Log. start ---
-                    if web_info.type.lower() == "tomcat" and re_tomcat:
-                        # 配置远程服务器的IP，帐号，密码，端口等，因做了双机密钥信任，所以不需要密码
-                        r_user = "app"
-                        r_ip = i.strip()
-                        r_port = 22
-                        r_log = "/apps/product/tomcat/logs/catalina.out"  # tomcat的启动日志路径
-                        cmd_rlog = "/usr/bin/ssh -p {port} {user}@{ip} /usr/bin/tail -f {log_path}".format(user=r_user, ip=r_ip, port=r_port, log_path=r_log)
-                        cmd_tstart = "/usr/bin/ssh -p {port} {user}@{ip} /apps/product/tomcat/bin/startup.sh".format(user=r_user, ip=r_ip, port=r_port)
-                        # cmd_tstop = "/usr/bin/ssh -p {port} {user}@{ip} /apps/product/script/tomcat_shutdown.sh".format(user=r_user, ip=r_ip, port=r_port)
-                        # cmd_kill_tail = "/usr/bin/ssh -p {port} {user}@{ip} /apps/product/script/kill_tail.sh".format(user=r_user, ip=r_ip, port=r_port)
-                        p_rlog = subprocess.Popen(cmd_rlog, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                        # p_stop = subprocess.Popen(cmd_tstop, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                        tom_stop_re = cli.cmd(tgt=r_ip, fun='state.sls', arg=['pkg.scripts.tomcat_shutdown'])
-                        request.websocket.send("Tomcat has stopped\n\n".encode('utf8'))
-                        p_start = subprocess.Popen(cmd_tstart, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-                        while p_start.poll() == None:
-                            start_l = p_start.stdout.readline()
-                            request.websocket.send(start_l.encode('utf8'))
-
-                        while p_rlog.poll() == None:
-                            re_log = p_rlog.stdout.readline()
-                            request.websocket.send(re_log.encode('utf8'))
-                            if "Server startup in" in re_log:
-                                # request.websocket.send("---END---\n".encode('utf8'))
-                                break
-                        kill_tail_re = cli.cmd(tgt=r_ip, fun='state.sls', arg=['pkg.scripts.kill_tail'])
-                    request.websocket.send("------更新完成！------\n\n".encode('utf8'))
-                    # --- Read Tomcat Log. end ---
+                # --- Read Tomcat Log. end ---
             break
         request.websocket.close()
 
@@ -202,42 +156,68 @@ def website_add(request):
                       type=rec_data['apptype'],git_url=rec_data['web_git_url'])
         web.save()
         cli = client.LocalClient()
-        git_pyscript = "/srv/salt/pkg/scripts/git_updatefile.py"
-        py_content = open(git_pyscript, 'r'.encode())
+        git_pyscript = "/srv/salt/pkg/scripts/git_update.py"
+        py_content = open(git_pyscript, 'r')
         py_lines = py_content.readlines()
         py_content.close()
         py_flen = len(py_lines)
         for num in range(py_flen):
-            if py_lines[num] == 'pro_git = ""\n':
-                py_lines[num] = py_lines[num].replace(py_lines[num], rec_data['web_git_url'] + "\n")
-            elif py_lines[num] == 'pro_path = ""\n':
-                py_lines[num] = py_lines[num].replace(py_lines[num], rec_data['web_path'] + "\n")
-        pyscript_name = rec_data['web_url']
-        new_py = open("/srv/salt/pkg/scripts/web_git/%s.py" % pyscript_name, "w".encode())
+            if py_lines[num].strip() == 'git_url = ""':
+                py_lines[num] = py_lines[num].replace(py_lines[num], "git_url = '%s'" % rec_data['web_git_url'] + "\n")
+            elif py_lines[num].strip() == 'git_path = ""':
+                if rec_data['apptype'] == "IIS":
+                    py_lines[num] = py_lines[num].replace(py_lines[num], "git_path = '%s'" % rec_data['web_path'].replace('\\','\\\\') + "\n")
+                else:
+                    py_lines[num] = py_lines[num].replace(py_lines[num], "git_path = '%s'" % rec_data['web_path'] + "\n")
+        pyscript_name = rec_data['web_url'].replace(".","_")
+        web_scr_dir = "/srv/salt/pkg/scripts/web_git/%s" % pyscript_name
+        if not os.path.exists(web_scr_dir + "/"):
+            if os.path.exists(web_scr_dir):
+                os.remove(web_scr_dir)
+            os.mkdir(web_scr_dir + "/")
+        new_py = open("/srv/salt/pkg/scripts/web_git/%s/%s.py" % (pyscript_name,pyscript_name), "w")
         new_py.writelines(py_lines)
         new_py.close()
         push_slsmode = "/srv/salt/pkg/scripts/git_update.sls"
-        sls_content = open(push_slsmode, 'r'.encode())
+        sls_content = open(push_slsmode, 'r')
         sls_lines = sls_content.readlines()
         sls_content.close()
         sls_flen = len(sls_lines)
         for num in range(sls_flen):
-            if "- name:" in sls_lines[num]:
-                if sys.platform == "win32":
-                    sls_lines[num] = sls_lines[num].strip() + " d:/product/web_git/%s.py\n" % pyscript_name
-                else:
-                    sls_lines[num] = sls_lines[num].strip() + " /apps/product/web_git/%s.py\n" % pyscript_name
             if "- source:" in sls_lines[num]:
-                sls_lines[num] = sls_lines[num].strip() + "%s.py\n" % pyscript_name
-        new_sls = open("/srv/salt/pkg/scripts/web_git/%s.sls" % pyscript_name, "w".encode())
+                if rec_data['apptype'] == "IIS":
+                    sls_lines[num-1] = "    " + sls_lines[num-1].strip() + " d:/product/web_git/%s.py\n" % pyscript_name
+                else:
+                    sls_lines[num-1] = "    " + sls_lines[num-1].strip() + " /apps/product/web_git/%s.py\n" % pyscript_name
+                sls_lines[num] = "    " + sls_lines[num].strip() + "%s/%s.py\n" % (pyscript_name,pyscript_name)
+        new_sls = open("/srv/salt/pkg/scripts/web_git/%s/%s.sls" % (pyscript_name,pyscript_name), "w")
         new_sls.writelines(sls_lines)
         new_sls.close()
-        top_sls = open("/srv/salt/top.sls", "a+".encode())
-        top_sls.write("pkg.scripts.web_git.%s\n".encode() % pyscript_name)
+        git_run_script = "/srv/salt/pkg/scripts/git_update_run.sls"
+        sls_git_content = open(git_run_script, 'r')
+        sls_git_lines = sls_git_content.readlines()
+        sls_git_content.close()
+        sls_git_flen = len(sls_git_lines)
+        for num in range(sls_git_flen):
+            if "- name:" in sls_git_lines[num]:
+                if rec_data['apptype'] == "IIS":
+                    sls_git_lines[num] = "    " + sls_git_lines[num].strip() + " d:/product/web_git/%s.py\n" % pyscript_name
+                else:
+                    sls_git_lines[num] = "    " + sls_git_lines[num].strip() + " /apps/product/web_git/%s.py\n" % pyscript_name
+                    sls_git_lines.append("    - user: app")
+        new_git_sls = open("/srv/salt/pkg/scripts/web_git/%s/%s_git.sls" % (pyscript_name,pyscript_name), "w")
+        new_git_sls.writelines(sls_git_lines)
+        new_git_sls.close()
+        top_sls = open("/srv/salt/top.sls", "a+")
+        top_data = ["    - pkg.scripts.web_git.%s.%s\n" % (pyscript_name,pyscript_name),"    - pkg.scripts.web_git.%s.%s_git\n" % (pyscript_name,pyscript_name)]
+        top_sls_lines = top_sls.readlines()
+        for top_item in top_data:
+            if top_item not in top_sls_lines:
+                top_sls.write(top_item)
         top_sls.close()
         for ip in rec_data['serverip'].split(','):
-            web.server_id.add(Servers.objects.get(ipaddress=ip.decode('utf8')))
-            cli.cmd(tgt=ip, fun="state.sls", arg=["pkg.scripts.web_git.%s" % pyscript_name])
+            web.server_id.add(Servers.objects.get(ipaddress=ip))
+            print cli.cmd(tgt=ip, fun="state.sls", arg=["pkg.scripts.web_git.%s.%s" % (pyscript_name,pyscript_name)])
         return HttpResponseRedirect('/salt/website_manage/')
 
 
@@ -301,7 +281,7 @@ def wesite_list(request):
         group_names = []
         for group in groups:
             group_names.append(group.name)
-        if "超级管理员" in group_names:
+        if "超级管理员".decode('utf8') in group_names:
             show_all = True
         if show_all:
             wesite = Website.objects.all()
